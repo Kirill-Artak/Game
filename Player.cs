25459439c0371d3b6b3720abf294655a6aa44c84
razzerone <Kirill.Sopegin@urfu.me>
2021-04-12 12:11:55 +0500

    Created player class

---
+++ Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace Game
+{
+    public class Player
+    {
+        public int Health { get; private set; }
+        public Point DeltaPosition { get; private set; }
+        public IWeapon Weapon { get; private set; }
+
+        private Player()
+        {
+            
+        }
+        
+        public Player MoveRight()
+        {
+            return new Player()
+            {
+                Health = Health,
+                DeltaPosition = DeltaPosition,
+                Weapon = Weapon
+            };
+        }
+
+        public void MoveLeft()
+        {
+            
+        }
+
+        public void Jump()
+        {
+            
+        }
+
+        public void Damage()
+        {
+            
+        }
+    }
+}
