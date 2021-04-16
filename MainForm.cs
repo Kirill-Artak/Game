ea9761ac3e144ff052670cd8b6e7493e8d2f04b8
razzerone <newrazzerone@gmail.com>
2021-04-17 00:24:48 +0500

    Created settings menu

--- MainForm.cs
+++ MainForm.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Drawing;
@@ -10,54 +11,39 @@ namespace Game
         private static Size menuButtonSize = new Size(180, 60);
         private static Font menuButtonFont = new Font("Arial", 16);
         private static Image backgroundImage = Image.FromFile(@"C:\Users\razzerone\Desktop\рисунки\a.png");
-        
-        private static Button NewGameButton = new Button()
-        {
-            Text = "Новая игра",
-            Size = menuButtonSize,
-            Font = menuButtonFont,
-            Anchor = AnchorStyles.Right,
-            FlatStyle = FlatStyle.Flat,
-        };
-        
-        private static Button SettingsButton = new Button()
-        {
-            Text = "Настройки",
-            Size = menuButtonSize,
-            Font = menuButtonFont,
-            Anchor = AnchorStyles.Right,
-            FlatStyle = FlatStyle.Flat,
-        };
-        
-        private static Button ExitButton = new Button()
-        {
-            Text = "Выход",
-            Size = menuButtonSize,
-            Font = menuButtonFont,
-            Anchor = AnchorStyles.Right,
-            FlatStyle = FlatStyle.Flat,
-        };
+
+        private static TableLayoutPanel MenuTable;
+        private static TableLayoutPanel SettingsTable;
+        private static TableLayoutPanel PauseTable;
         
         public MainForm()
         {
             Size = new Size(1280, 720);
             FormBorderStyle = FormBorderStyle.FixedDialog;
 
-            var table = MenuBuilder();
-            
+            MenuTable = MenuBuilder(
+                ButtonBuilder("Новая игра", (s, e) => {
+                    Controls.Remove(MenuTable);
+                    Paint += RunGame;
+                }),
+                ButtonBuilder("Настройки", (s, e) =>
+                {
+                    Controls.Remove(MenuTable);
+                    Controls.Add(SettingsTable);
+                }),
+                ButtonBuilder("Выход", (s, e) => Application.Exit())
+            );
 
-            NewGameButton.Click += (sender, args) =>
-            {
-                Controls.Remove(table);
-                
-                Paint += RunGame;
-            };
+            SettingsTable = MenuBuilder(
+                CheckBoxBuilder("Полный экран", (s, e) => {}),
+                TrackBarBuilder("Громкость", (s, e) => {}),
+                ButtonBuilder("Назад", (s, e) =>
+                {
+                    Controls.Remove(SettingsTable);
+                    Controls.Add(MenuTable);
+                }));
             
-            SettingsButton.Click += (sender, args) => Controls.Remove(table);
-            
-            ExitButton.Click += (sender, args) => Application.Exit();
-            
-            Controls.Add(table);
+            Controls.Add(MenuTable);
         }
 
         private void RunGame(object sender, PaintEventArgs e)
@@ -70,14 +56,8 @@ namespace Game
             
         }
 
-        private static TableLayoutPanel MenuBuilder()
+        private static TableLayoutPanel MenuBuilder(params Control[] buttons)
         {
-            var menuControls = new List<Button>();
-            
-            menuControls.Add(NewGameButton);
-            menuControls.Add(SettingsButton);
-            menuControls.Add(ExitButton);
-            
             var table = new TableLayoutPanel();
             
             table.BackgroundImage = backgroundImage;
@@ -96,20 +76,80 @@ namespace Game
             var placeHolder = new Panel() { BackColor = Color.Transparent };
             
             table.Controls.Add(placeHolder, 1, 0);
-            for (int i = 0; i < 3; i++)
-            {
-                var button = i;
-
-                menuControls[button].GotFocus += (sender, args) => menuControls[button].BackColor = Color.DarkSlateBlue;
-                menuControls[button].MouseEnter += (sender, args) => menuControls[button].BackColor = Color.DarkSlateBlue;
-
-                menuControls[button].LostFocus += (sender, args) => menuControls[button].BackColor = DefaultBackColor;
-                menuControls[button].MouseLeave += (sender, args) => menuControls[button].BackColor = DefaultBackColor;
-                
-                table.Controls.Add(menuControls[button], 0, button + 1);
-            }
             
+            for (int i = 0; i < buttons.Length; i++)
+                table.Controls.Add(buttons[i], 0, i + 1);
+
             return table;
+        }
+        
+        private static Button ButtonBuilder(string name, EventHandler action)
+        {
+            var button = new Button()
+            {
+                Text = name,
+                Size = menuButtonSize,
+                Font = menuButtonFont,
+                ForeColor = Color.DarkGoldenrod,
+                BackColor = Color.Transparent,
+                Anchor = AnchorStyles.Right,
+                FlatStyle = FlatStyle.Flat,
+            };
+            
+            button.GotFocus += (sender, args) => button.BackColor = Color.DarkSlateBlue;
+            button.MouseEnter += (sender, args) => button.BackColor = Color.DarkSlateBlue;
+
+            button.LostFocus += (sender, args) => button.BackColor = Color.Transparent;
+            button.MouseLeave += (sender, args) => button.BackColor = Color.Transparent;
+
+            button.Click += action;
+
+            return button;
+        }
+
+        private static TrackBar TrackBarBuilder(string name, EventHandler scroll)
+        {
+            var trackBar = new TrackBar()
+            {
+                Text = name,
+                Size = menuButtonSize,
+                Font = menuButtonFont,
+                Anchor = AnchorStyles.Right,
+                //BackColor = Color.Black,
+                //ForeColor = Color.DarkGoldenrod,
+                Minimum = 0,
+                Maximum = 4,
+                TickFrequency = 1,
+            };
+            
+            trackBar.Scroll += scroll;
+
+            return trackBar;
+        }
+
+        private static CheckBox CheckBoxBuilder(string name, EventHandler checkedChanged)
+        {
+            var checkBox = new CheckBox()
+            {
+                Text = name,
+                Size = menuButtonSize,
+                Font = menuButtonFont,
+                ForeColor = Color.DarkGoldenrod,
+                FlatStyle = FlatStyle.Flat,
+                Anchor = AnchorStyles.Right,
+                BackColor = Color.Transparent,
+                Checked = false,
+            };
+            
+            checkBox.GotFocus += (sender, args) => checkBox.BackColor = Color.DarkSlateBlue;
+            checkBox.MouseEnter += (sender, args) => checkBox.BackColor = Color.DarkSlateBlue;
+
+            checkBox.LostFocus += (sender, args) => checkBox.BackColor = Color.Transparent;
+            checkBox.MouseLeave += (sender, args) => checkBox.BackColor = Color.Transparent;
+
+            checkBox.CheckedChanged += checkedChanged;
+
+            return checkBox;
         }
     }
 }
